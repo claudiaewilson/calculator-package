09f6af8c91aefd6b1fdfc373e3bc3431e00f7dab
Lehamgreeves <lgreeves@alxafrica.com>
2026-06-24 16:09:26 +0200

    Leham's first, stats file and example file

--- calculator_package/__init__.py
+++ calculator_package/__init__.py
@@ -7,3 +7,5 @@ This package provides reusable calculator functions.
 from calculator_package.basic import BasicCalculator
 
 from calculator_package.advanced import AdvancedCalculator
+
+from calculator_package.statistics import ScientificCalculator
